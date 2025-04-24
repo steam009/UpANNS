b0122d13b171e98b577def9916e2a12af767dcca
steam009 <874775571@qq.com>
2025-04-24 12:04:26 +0800

    cache version

--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
 export NR_DPUS=896
-export NR_TASKLETS=14
+export NR_TASKLETS=16
 export MS=20
 export KSUB=256
 export DSUB=5
 export MAX_DPU_STORE_SIZE=1366000
 export CODE_SIZE=20
 export TOPK=10
-export MAX_Q_O=550
-export MAX_PROBE_NUM=792
+export MAX_Q_O=1008
+export MAX_PROBE_NUM=2099
 export DIMM=104
-export MAX_DPU_ID=72
+export MAX_DPU_ID=10
 export CODEBOOK_SIZE=8192
-export ETD=32
-export NPROBS=64
-export BS=100
+export ETD=16
+export NPROBS=16
 # export DEBUG=1
 
 make clean
